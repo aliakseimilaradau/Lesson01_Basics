9b62d03c1a618d7ef231bd1cf2e22082e9988238
aliakseimilaradau <leksa8858@gmail.com>
2026-07-07 16:55:31 +0300

    Ввод данных пользователем через консоль, условия if-else

---
+++ Lesson02/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Как тебя зовут?");
+string userName = Console.ReadLine();
+
+Console.WriteLine($"Привет, {userName}");
+
+Console.WriteLine("Сколько тебе лет?");
+string ageInput = Console.ReadLine();
+
+int userAge = int.Parse(ageInput);
+
+if (userAge < 18)
+{
+    Console.WriteLine("Ты несовершенолетний");
+}
+else if (userAge <= 65)
+{
+    Console.WriteLine("Ты совершенолетний");
+}
+else
+{
+    Console.WriteLine("Ты можешь выйти на пенсию");
+}
