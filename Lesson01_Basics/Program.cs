b596d23f59ec766eb92b2e056ea6041243326545
aliakseimilaradau <leksa8858@gmail.com>
2026-07-07 15:59:00 +0300

    Базовые переменные и типы данных

---
+++ Lesson01_Basics/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+string name = "Андрей";   // текст — тип string
+int age = 25;             // целое число — тип int
+double height = 1.78;     // дробное число — тип double
+bool isStudent = true;    // да/нет — тип bool
+string favoriteColor = "Black";
+
+Console.WriteLine("Привет, меня зовут " + name);
+Console.WriteLine("Мне " + age + " лет");
+Console.WriteLine("Мой рост: " + height + " м");
+Console.WriteLine("Я студент: " + isStudent);
+Console.WriteLine($"Мой любымый цвет {favoriteColor}");
