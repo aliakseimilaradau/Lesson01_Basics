8e2debef675ebb00ed00b4e1493b0d02c60dc7a8
aliakseimilaradau <leksa8858@gmail.com>
2026-07-10 16:48:13 +0300

    Проверка на простое число

---
+++ Lesson04_ForLoop/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+int AskNumber ()
+{
+    Console.WriteLine("Введите число:");
+    string input = Console.ReadLine();
+    int number = int.Parse(input);
+
+    while (number <= 0)
+    {
+        Console.WriteLine("Введите положительное число");
+        input = Console.ReadLine();
+        number = int.Parse(input);
+    }
+    return number;
+}
+
+int number = AskNumber ();
+int sum = 0;
+
+for (int i = 1; i <= number; i++)
+{
+    int remainder = number % i;
+        if (remainder == 0)
+    {
+        Console.WriteLine($"Делитель: {i}");
+        if (i != number)
+        {
+            sum = (sum + i);
+        }
+    }
+}
+
+if (sum == number)
+{
+    Console.WriteLine("Это совершенное число!");
+}
+else
+{
+    Console.WriteLine($"Сумма: {sum}");
+}
