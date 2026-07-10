4b24c746425bb6cac84d183b9763097cfdbe90f4
aliakseimilaradau <leksa8858@gmail.com>
2026-07-10 12:05:49 +0300

    Калькулятор индекса массы тела

---
+++ Lesson03_BMI/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+double AskWeight()
+{
+    Console.WriteLine("Укажите ваш вес в килограммах");
+    string inputWeight = Console.ReadLine();
+    double weight = double.Parse(inputWeight);
+    while (weight <= 0)
+    {
+        Console.WriteLine("Вес указан не верно. Повторите ввод.");
+        string input = Console.ReadLine();
+        double userWeight = double.Parse(input);
+    }
+    return weight;
+}
+
+double AskHeight()
+{
+    Console.WriteLine("Укажите ваш рост в метрах");
+    string inputHeight = Console.ReadLine();
+    double height = double.Parse(inputHeight);
+    while (height <= 0)
+    {
+        Console.WriteLine("Рост указан не верно. Повторите ввод.");
+        string input = Console.ReadLine();
+        double userHeight = double.Parse(input);
+    }
+    return height;
+}
+
+double weight = AskWeight();
+double height = AskHeight();
+
+double bmi = weight/(height * height);
+
+if (bmi < 18.5)
+{
+    Console.WriteLine($"Недостаточный вес. bmi = {bmi}");
+}
+else if (bmi < 24.9)
+{
+    Console.WriteLine($"Норма. bmi = {bmi}");
+}
+else  if (bmi < 29.9)
+{
+    Console.WriteLine($"Избыточный вес. bmi = {bmi}");
+}
+else
+{
+    Console.WriteLine($"Ожирение. bmi = {bmi}");
+}
