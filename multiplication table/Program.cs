d6c1104e617e5b469f6ed3e37fd258e1ea270b36
aliakseimilaradau <leksa8858@gmail.com>
2026-07-10 13:45:06 +0300

    Таблица умножения на число, которое вводит пользователь

---
+++ multiplication table/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.OutputEncoding =  System.Text.Encoding.UTF8;
+
+Console.WriteLine("Введите число:");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+
+for (int i = 1; i <= 10; i++)
+{
+        Console.WriteLine($"{number} x {i} = {number * i}");
+}
