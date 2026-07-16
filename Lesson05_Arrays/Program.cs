3180ce35cea7887dac3fc569e1d7e199bfeac36b
aliakseimilaradau <leksa8858@gmail.com>
2026-07-16 12:34:38 +0300

    Расчет среднего, минимального, максимального балла

---
+++ Lesson05_Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+int[] grades = { 75, 34, 56, 90, 65, 100, 1 };
+int sum = 0;
+int max = 0;
+int min = 100;
+
+for (int i = 0; i < grades.Length; i++)
+{
+    Console.WriteLine($"Оценка {i + 1}: {grades[i]}");
+    sum = sum + grades[i];
+
+    if (grades[i] > max)
+    {
+        max = grades[i];
+    }
+
+    if (grades[i] < min)
+    {
+        min = grades[i];
+    }
+}
+
+double averageGrades = Math.Round(((double)sum / grades.Length), 1, MidpointRounding.AwayFromZero);
+
+Console.WriteLine($"Сумма баллов: {sum}");
+Console.WriteLine($"Средний балл: {averageGrades}");
+Console.WriteLine($"Максимальный балл: {max}");
+Console.WriteLine($"Минимальный балл: {min}");
